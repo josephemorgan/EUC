08bab845f5782f9c746bc293c6c3b189e521b8cd
Joe <josephemorgan91@gmail.com>
2020-03-24 20:29:09 -0700

    EUC-1 Create Min Window

--- src/main/java/edu/csus/csc131/euc/App.java
+++ src/main/java/edu/csus/csc131/euc/App.java
@@ -1,13 +1,10 @@
 package edu.csus.csc131.euc;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		MainWindow testwindow = new MainWindow();
+		testwindow.createWindow();
     }
 }
---
+++ src/main/java/edu/csus/csc131/euc/MainWindow.java
@@ -0,0 +1,13 @@
+package edu.csus.csc131.euc;
+
+import javax.swing.*;
+
+public class MainWindow
+{
+	public static void createWindow() {
+		JFrame f = new JFrame();
+
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+}
