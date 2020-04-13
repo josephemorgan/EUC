1568a815f5ce2b89f55d948e3e74f886e246027b
sandhara <sandhara@ecs.csus.edu>
2020-04-13 15:26:49 -0700

    Updated Parser class to convert to LocalDate format, created ParserTest class to test Parser class

--- src/main/java/edu/csus/csc131/euc/Parser.java
+++ src/main/java/edu/csus/csc131/euc/Parser.java
@@ -5,7 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
+
+import static java.time.ZoneId.systemDefault;
 
 /**
  * The parser class is used to retrieve relevant data from a JSON file to be used by the Day class
@@ -18,17 +24,25 @@ import java.util.ArrayList;
 
 public class Parser {
 
+    private static final int HOURS_IN_DAY = 24;
+
     private ObjectMapper mapper = new ObjectMapper();
 
     private int userId;
     private String unit;
     private String siteTimeZoneId;
 
-    private ArrayList<String> startTimes = new ArrayList();
-    private ArrayList<String> endTimes = new ArrayList();
-    private ArrayList values = new ArrayList();
+    private ArrayList<String> startTimesString = new ArrayList();
+    private ArrayList<String> endTimesString = new ArrayList();
+    private ArrayList<String> valuesString = new ArrayList();
 
-    public Parser(){}
+    private ArrayList<LocalDate> startTimes = new ArrayList();
+    private ArrayList<LocalDate> endTimes = new ArrayList();
+
+    private double[] values = new double[HOURS_IN_DAY];
+
+    public Parser() {
+    }
 
     /**
      * Use this method to update the Parser object with data from the given file.
@@ -37,6 +51,7 @@ public class Parser {
      * @param filePath the file path to the JSON file to be read from
      * @throws IOException
      */
+
     public void fetchData(String filePath) {
 
         try {
@@ -44,37 +59,84 @@ public class Parser {
             //Define JsonNode to read data from
             JsonNode root = mapper.readTree(new File(filePath));
 
-            //Assignment of simple values
+            //Assignment of simple valuesString
             userId = root.get("userId").asInt();
             unit = root.get("unit").asText();
             siteTimeZoneId = root.get("siteTimeZoneId").asText();
 
-            //Assignment of array values under the "reads" object
-            root.findValuesAsText("startTime", startTimes);
-            root.findValuesAsText("endTime", endTimes);
-            root.findValues("value", values);
+            //Assignment of array valuesString under the "reads" object
+            root.findValuesAsText("startTime", startTimesString);
+            root.findValuesAsText("endTime", endTimesString);
+            root.findValuesAsText("value", valuesString);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        convertTimeArrays();
+        convertValueArray();
     }
 
     /**
      * Print out data currently stored in Parser object.
      */
 
-    public void printData(){
+    public void printData() {
         System.out.println("UserId: " + userId);
         System.out.println("Unit: " + unit);
         System.out.println("siteTimeZoneId" + siteTimeZoneId);
 
-        for(int i = 0; i < startTimes.size(); i++){
+        for (int i = 0; i < startTimesString.size(); i++) {
             System.out.println();
             System.out.println("Hour: " + i);
-            System.out.println("startTime: " + startTimes.get(i));
-            System.out.println("endTime: " + endTimes.get(i));
-            System.out.println("value: " + values.get(i));
+            System.out.println("startTime: " + startTimesString.get(i));
+            System.out.println("endTime: " + endTimesString.get(i));
+            System.out.println("value: " + valuesString.get(i));
             System.out.println();
+        }
+    }
+
+    //Converts the arrays which contain time in String format to LocalDate format
+    private void convertTimeArrays() {
+
+        for (String stringTime : startTimesString) {
+            startTimes.add(convertTimeString(stringTime));
+        }
+
+        for (String stringTime : endTimesString) {
+            endTimes.add(convertTimeString(stringTime));
+        }
+
+    }
+
+    //Converts a time in String format to LocalDate format
+    private LocalDate convertTimeString(String in) {
+
+        in = in.substring(0, 19);    //Truncate sub-seconds and time zone data from time string
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        LocalDate out = null;
+
+        try {
+            //Parses date in String format into a Date object
+            Date date = format.parse(in);
+
+            //Assigns the value of the date to the LocalDate object, uses local time zone
+            out = date.toInstant().atZone(systemDefault()).toLocalDate();
+        } catch (ParseException e) {
+            System.out.println("Time String parsing exception.");
+            e.printStackTrace();
+        }
+
+        return out;
+    }
+
+    //Converts String valuesString for usage into doubles
+    private void convertValueArray(){
+        int i = 0;
+        for (String stringValue  : valuesString) {
+            values[i] = Double.parseDouble(stringValue);
+            i++;
         }
     }
 
@@ -90,15 +152,19 @@ public class Parser {
         return siteTimeZoneId;
     }
 
-    public ArrayList<String> getStartTimes() {
+    public ArrayList<LocalDate> getStartTimes() {
         return startTimes;
     }
 
-    public ArrayList<String> getEndTimes() {
+    public ArrayList<LocalDate> getEndTimes() {
         return endTimes;
     }
 
-    public ArrayList getValues() {
+    public ArrayList<String> getValuesString() {
+        return valuesString;
+    }
+
+    public double[] getValues() {
         return values;
     }
 }
