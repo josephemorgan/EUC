bf8d108d17e9372b77b91e936488f8f00c9456e8
Joe <josephemorgan91@gmail.com>
2020-04-13 15:25:55 -0700

    Created Usage class and modified Day to use Usage and Rates

---
+++ src/main/java/edu/csus/csc131/euc/Usage.java
@@ -0,0 +1,38 @@
+package edu.csus.csc131.euc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Usage {
+
+    private static final int HOURS_IN_DAY = 24;
+    private Map<Integer, Double> u;
+
+    public Usage() {
+        u = initMap();
+    }
+
+    public double getUsage(int i) {
+        return u.get(i);
+    }
+
+    public void setUsage(int i, double r) {
+        u.replace(i, r);
+    }
+
+    @Override
+    public String toString() {
+        return "Usage{" +
+                "Usage=" + u +
+                '}';
+    }
+
+    private static Map<Integer, Double> initMap() {
+        Map<Integer, Double> m = new HashMap<>();
+        for (int i = 0; i < HOURS_IN_DAY; ++i) {
+            m.put(i, 0.0);
+        }
+        return m;
+    };
+
+}
